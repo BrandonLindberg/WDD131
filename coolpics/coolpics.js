6e1613947c491b1db7024dbc0e10a5bfca051cc8
Brandon Lindberg <brandon.s.lindberg@gmail.com>
2025-10-21 11:29:04 -0600

    cool pics final

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,4 +1,6 @@
 const menuButton = document.querySelector("button");
+const galleryClick = document.querySelector(".gallery");
+const mainBody = document.querySelector("body");
 
 function displayMenu() {
     const menu = document.querySelector("nav");
@@ -7,14 +9,28 @@ function displayMenu() {
 
 function viewHandler(event) {
     const target = event.target;
-    
+    let targetSrc = target.getAttribute("src");
+    const newSrc = targetSrc.split("-");
+
+    const splitImg = newSrc[0]
+    let newImg = splitImg + "-full.jpeg"
+
+    mainBody.insertAdjacentHTML("afterbegin", viewerTemplate(newImg, "full image"))
+    const closeView = document.querySelector(".close-viewer")
+
+    closeView.addEventListener("click", closeViewer);
 }
 
-function viewerTemplate() {
+function viewerTemplate(image, alt) {
     return `<div class="viewer">
         <button class="close-viewer">X</button>
         <img src="${image}" alt="${alt}">
     </div>`;
 }
 
+function closeViewer() {
+    mainBody.querySelector(".viewer").remove();
+}
+
 menuButton.addEventListener("click", displayMenu);
+galleryClick.addEventListener("click", viewHandler);
